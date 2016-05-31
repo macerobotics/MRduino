4c47657d64715bc190870a9fb082bf5f1381de7a
macerobotics <contact@macerobotics.com>
2016-05-31 15:22:34 +0200

    update

--- Software/LibArduino/mrduino.h
+++ Software/LibArduino/mrduino.h
@@ -1,13 +1,22 @@
-
-
+/**
+  ******************************************************************************
+  * @file    mrduino.cpp
+  * @author  Mace Robotics
+  * @version 0.2
+  * @date    31/06/2016
+  * @brief
+  *
+ *******************************************************************************/
 
 #ifndef _MRduino_H
 #define _MRduino_H
 
+void initRobot();
+
 void led(int led, int state);
 void ledToggle(int led);
 
-void firmwareVersion();
+float firmwareVersion();
 
 
 void forward(int speed);
@@ -17,13 +26,25 @@ void turnRight(int speed);
 
 int proxSensor(int number);
 int ambiantLight(int number);
+int groundSensor(int number);
 
 void controlEnable();
 void controlDisable();
 
+int readSwitch();
+
+float battery();
+float temperature();
+
+void forwardC(int speed, int distance);
+void backC(int speed, int distance);
+void turnRightC(int speed, int distance);
+void turnLeftC(int speed, int distance);
+
 void stop();
 
 int readData();
+float readFloatData();
 
 
 #endif
